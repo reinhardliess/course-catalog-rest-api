ce08015ccb8e7e65004e090955a67c3e9f8348ca
Reinhard Liess <reinhard.liess@gmail.com>
2019-12-28 12:33:03 +0100

    add global css, header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends React.PureComponent {
+  render() {
+    return (
+      <div className="header">
+        <div className="bounds">
+          <h1 className="header--logo">Courses</h1>
+          <nav>
+            <Link className="signup" to="/signup">
+              Sign Up
+            </Link>
+            <Link className="signin" to="/signin">
+              Sign In
+            </Link>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
