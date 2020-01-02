c88f3d4b28a0a5046a938cfc221fc5f6f23998f9
Reinhard Liess <reinhard.liess@gmail.com>
2020-01-02 10:47:59 +0100

    change header to functional component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,41 +1,38 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import withContext from '../Context';
 
-class Header extends PureComponent {
-  render() {
-    const { context } = this.props;
-    const { authenticatedUser } = context;
+const Header = ({ context }) => {
+  const { authenticatedUser } = context;
 
-    return (
-      <div className="header">
-        <div className="bounds">
-          <h1 className="header--logo">Courses</h1>
-          <nav>
-            {authenticatedUser ? (
-              <>
-                <span>
-                  Welcome,{' '}
-                  {`${authenticatedUser.firstName} ${authenticatedUser.lastName}`}
-                  !
-                </span>
-                <Link to="/signout">Sign Out</Link>
-              </>
-            ) : (
-              <>
-                <Link className="signup" to="/signup">
-                  Sign Up
-                </Link>
-                <Link className="signin" to="/signin">
-                  Sign In
-                </Link>
-              </>
-            )}
-          </nav>
-        </div>
+  return (
+    <div className="header">
+      <div className="bounds">
+        <h1 className="header--logo">Courses</h1>
+        <nav>
+          {authenticatedUser ? (
+            <>
+              <span>
+                Welcome,{' '}
+                {`${authenticatedUser.firstName} ${authenticatedUser.lastName}`}
+                !
+              </span>
+              <Link to="/signout">Sign Out</Link>
+            </>
+          ) : (
+            <>
+              <Link className="signup" to="/signup">
+                Sign Up
+              </Link>
+              <Link className="signin" to="/signin">
+                Sign In
+              </Link>
+            </>
+          )}
+        </nav>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default withContext(Header);
