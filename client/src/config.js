e03b3e3f1c95c56f087ab6950cd7998f5e462ec4
Reinhard Liess <reinhard.liess@gmail.com>
2020-02-07 21:22:08 +0100

    update config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export default {
-  // apiBaseUrl: 'http://localhost:5000/api',
-  apiBaseUrl: 'https://rliess-course-api.herokuapp.com/api',
+  apiBaseUrl: 'http://localhost:5000/api',
+  // apiBaseUrl: 'https://rliess-course-api.herokuapp.com/api',
 };
